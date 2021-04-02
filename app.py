146695b1463cc009288e8b0711980a15f586a4d8
zim <1399316847@qq.com>
2021-04-02 14:12:52 +0800

    commit

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask
-app=Flask(__name__)
+import os
+import logging
+import redis
+import gevent
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+REDIS_URL = os.environ['REDIS_URL']
+REDIS_CHAN = 'chat'
+
+app = Flask(__name__)
+app.debug = 'DEBUG' in os.environ
+
 
 @app.route('/')
 def hello_world():
