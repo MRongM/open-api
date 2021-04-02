ecebc67c99f1051dcdd55341c7a7fedf111ccb94
zim <1399316847@qq.com>
2021-04-02 14:15:19 +0800

    commit

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ import os
 import logging
 import redis
 import gevent
-from flask import Flask, render_template
-from flask_sockets import Sockets
-
-REDIS_URL = os.environ['REDIS_URL']
-REDIS_CHAN = 'chat'
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
